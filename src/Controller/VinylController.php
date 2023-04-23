17004fc1100e355065dc3f925357db3b2466938d
Jack Katzler <jack.katzler@gmail.com>
2023-04-24 00:17:17 +0200

    Create a list of songs and artists with twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,28 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
    public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Easier To Run', 'artist' => 'Linkin Park'],
+            ['song' => 'Numb', 'artist' => 'Linkin Park'],
+            ['song' => 'Jesus of Suburbia', 'artist' => 'Green Day'],
+            ['song' => 'Dark Side', 'artist' => 'Blind Channel']
+            ];
+
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PBs & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
